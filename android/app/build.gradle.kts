810a7a583ff25bf5616dc0dd011ccb2b34f40638
Fabrone <lubangafabron@gmail.com>
2025-09-04 23:48:23 +0300

    Initial changes to gradle kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.jvalmacis.coffeecore"
         minSdk = flutter.minSdkVersion.toInt()
         targetSdk = flutter.targetSdkVersion.toInt()
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         multiDexEnabled = true
     }
 
